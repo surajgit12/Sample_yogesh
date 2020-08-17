4720148a7fd6b7a77598fb2625b0a5de34a9ce21
surajgit12 <surajgit12@gmail.com>
2020-08-17 18:02:34 +0530

    added subtraction code and home page

---
+++ src/main/java/Subtraction.java
@@ -0,0 +1,17 @@
+
+public class Subtraction {
+	
+	static int a = 10;
+	static int b =20;
+	
+	public static void main(String[] args) {
+
+		Addition t = new Addition();
+		System.out.println(t.a);
+		System.out.println(t.b);
+		System.out.println("Subtraction is executed successfully----->" + (b-a));
+
+	}
+
+
+}
