f22a574cbb50f09f040270cde264190efb6cef56
surajgit12 <surajgit12@gmail.com>
2020-08-17 17:56:04 +0530

    Added yogesh project data

---
+++ src/main/java/Addition.java
@@ -0,0 +1,17 @@
+
+public class Addition {
+
+	
+	static int a = 10;
+	static int b =20;
+	
+	public static void main(String[] args) {
+
+		Addition t = new Addition();
+		System.out.println(t.a);
+		System.out.println(t.b);
+		System.out.println("Addition is executed successfully----->" + (a+b));
+
+	}
+
+}
---
+++ src/test/java/src_test/Sample_Test.java
@@ -0,0 +1,16 @@
+package src_test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Sample_Test {
+	
+	
+	@Test
+	void m1() {
+		
+		Assert.assertTrue(true);
+		System.out.println("Test Cases is passed");
+	}
+
+}
